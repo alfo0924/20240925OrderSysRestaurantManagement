68a20d03df9a5c1d0deb2b4a24f371623e7fde69
alfo0924 <halo0924@yahoo.com.tw>
2024-09-25 21:14:21 +0800

    v1.01 init cnp

---
+++ src/main/java/org/example/_20240925ordersysrestaurantmanagement/Application.java
@@ -0,0 +1,13 @@
+package org.example._20240925ordersysrestaurantmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
